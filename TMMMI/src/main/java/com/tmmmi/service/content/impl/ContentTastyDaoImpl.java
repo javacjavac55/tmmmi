2c82d55ed1dc9ebd8ce59c1d4c7c1b7e65a046af
Bit <Bit@DESKTOP-0NN2K5B>
2019-01-24 09:52:28 +0900

    Alex : TastyDao 이름 수정

--- TMMMI/src/main/java/com/tmmmi/service/content/impl/ContentTastyDaoImpl.java
+++ TMMMI/src/main/java/com/tmmmi/service/content/impl/ContentTastyDaoImpl.java
@@ -1,24 +1,16 @@
 package com.tmmmi.service.content.impl;
 
-import com.tmmmi.service.content.ContentDao;
-import com.tmmmi.service.domain.ContentSetting;
 import com.tmmmi.service.domain.ContentTasty;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.List;
 
-import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository("contentTastyDaoImpl")
-public class ContentTastyDaoNaverImpl extends ContentDaoAdaptor {
+public class ContentTastyDaoImpl extends ContentDaoAdaptor {
 	
 	///Constructor
-	public ContentTastyDaoNaverImpl() {
+	public ContentTastyDaoImpl() {
 	}
 	
 	///Method
@@ -91,12 +83,6 @@ public class ContentTastyDaoNaverImpl extends ContentDaoAdaptor {
         } catch (Exception e) {
             System.out.println(e);
         }
-<<<<<<< HEAD
-        
-        ArrayList list = new ArrayList<>();
-		
-        return null;
-=======
 	}*/
 
 }
