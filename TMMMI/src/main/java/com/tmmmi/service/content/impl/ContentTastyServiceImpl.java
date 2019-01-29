cfd878037954deb574e1c1b1580ee7f62b9bf226
Bit <Bit@Bitcamp>
2019-01-29 21:00:12 +0900

    Tom : 할 일 UI 모어 해야함, 워드클라우드, 할일 주차그래프, 컨텐츠박스합치기

--- TMMMI/src/main/java/com/tmmmi/service/content/impl/ContentTastyServiceImpl.java
+++ TMMMI/src/main/java/com/tmmmi/service/content/impl/ContentTastyServiceImpl.java
@@ -1,11 +1,15 @@
 package com.tmmmi.service.content.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import org.springframework.stereotype.Service;
 
 import com.tmmmi.service.content.ContentDao;
+import com.tmmmi.service.domain.ContentSetting;
+import com.tmmmi.service.domain.ContentTasty;
 
 @Service("contentTastyServiceImpl")
 public class ContentTastyServiceImpl extends ContentServiceAdaptor {	
@@ -18,8 +22,13 @@ public class ContentTastyServiceImpl extends ContentServiceAdaptor {
 	}
 	
 	//Constructor
-	public ContentTastyServiceImpl() {
+	public ContentTastyServiceImpl(){
 		System.out.println(this.getClass());
 	}
 	
+	///Method
+	public List<Object> getContentTastyList(ContentSetting contentSetting, int index)throws Exception {
+		
+		return contentTastyDao.getContentTastyList(contentSetting, index);
+	}
 }
--- TMMMI/src/main/java/com/tmmmi/web/ContentTastyRestController.java
+++ TMMMI/src/main/java/com/tmmmi/web/ContentTastyRestController.java
@@ -6,18 +6,18 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tmmmi.service.content.ContentService;
 import com.tmmmi.service.contentsetting.ContentSettingService;
 
-@Controller
-@RequestMapping("/contentTasty/*")
-public class ContentTastyController {
+@RestController
+@RequestMapping("/contentTastyRest/*")
+public class ContentTastyRestController {
 	
+	///Field
 	@Autowired
 	@Qualifier("contentTastyServiceImpl")
 	private ContentService contentTastyService;	
@@ -26,16 +26,14 @@ public class ContentTastyController {
 	@Qualifier("contentSettingServiceImpl")
 	private ContentSettingService contentSettingService;
 	
-	@RequestMapping(value="/contentTastyList", method=RequestMethod.GET)
-	public ModelAndView getContentList(HttpSession session)throws Exception {
+	///Constructor
+	public ContentTastyRestController() {
+		System.out.println(this.getClass());
+	}
+	@RequestMapping(value="/restContentTastyList", method=RequestMethod.GET)
+	public List<Object> getContentList(HttpSession session)throws Exception {
 		
 		int userNo = (int) session.getAttribute("userNo");
-		List<Object> contentList = contentTastyService.getContentTastyList(contentSettingService.getContentSetting(userNo), 0);
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("contentTastyList", contentList);
-		modelAndView.setViewName("/content/tastyContent.jsp");
-		System.out.println(contentList);
-		System.out.println(contentList.size());
-		return modelAndView;
+		return contentTastyService.getContentTastyList(contentSettingService.getContentSetting(userNo), 0);
 	}
 }
